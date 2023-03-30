3b53026560c6cb2b6b5876ff2ded46a725fcae07
abhishek raj goswami <goswamiabhishekraj@gmail.com>
2023-03-30 14:38:52 +0530

    second

---
+++ src/style.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  heading: {
+    color: "#35919d",
+    backgroundColor: "#b9f6ca",
+  },
+  area: {
+    backgroundColor: "#0b090a",
+  },
+}));
